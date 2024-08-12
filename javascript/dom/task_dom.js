3fbe70119417f7bb9fb94a2a6fa237b0e3c7d468
AdithyaKrishnan-code <jamesmacaroni69@gmail.com>
2024-08-12 16:48:21 +0530

    added new tasks

--- javascript/dom/task_dom.js
+++ javascript/dom/task_dom.js
@@ -1,6 +1,8 @@
 let bd = document.body
 let div = document.createElement("div")
 let div2 = document.createElement("div2")
+div.append(div2)
+
 
 
 let cd = document.getElementById("heading")
@@ -9,10 +11,19 @@ cd.innerHTML = "<h1>goodbye</h1>"
 cd.style.color="red"
 
 
-bd.append(div)
-bd.append(div2)
+
+
+
 
 div.style.backgroundColor = "black"
 div.style.width="100px"
 div.style.height = "100px"
+
+div2.innerText = "good morning"
+div2.style.color = "white"
+
+bd.append(div)
+
+
+
 
---
+++ javascript/event_listener/event_listenere.js
@@ -0,0 +1,8 @@
+let button_1 = document.querySelector(".but1")
+let button_2 = document.querySelector(".but2")
+
+
+button_1.addEventListener("click", () =>{console.log("called_button 1");})
+button_2.addEventListener("click", () =>{console.log("called button 2");})
+
+ 
