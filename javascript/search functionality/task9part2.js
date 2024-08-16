926e6bd599c414bdc44f2c0c4f408e1e1c782d62
AdithyaKrishnan-code <jamesmacaroni69@gmail.com>
2024-08-16 16:11:48 +0530

    added task9part2 completed with added enter button as a shortcut for adding tasks and staarted tas 10

--- javascript/search functionality/task9part2.js
+++ javascript/search functionality/task9part2.js
@@ -5,32 +5,32 @@ let delete_button = document.getElementById("deletebutton")
 
 // array of tasks
 
-let things_to_do =[]
+let things_to_do = []
 
 // ---------------------------------------------------------------------------
 
 // function to add  aalong with buttons tasks to the array
 
-let d_b 
+let d_b
 
-function add_tasks(){
-    
+function add_tasks() {
+
     things_to_do.push(task.value)
-    
-    task_list.innerHTML =""
-    
+
+    task_list.innerHTML = ""
+
     task.value = ""
-    
-    
-    for(let i = 0 ; i<things_to_do.length; i++){
-        
+
+
+    for (let i = 0; i < things_to_do.length; i++) {
+
         let items = document.createElement("div")
 
-        let text = document.createElement("p")
-        
-        items.setAttribute("id", i  )
+        let text = document.createElement("div")
 
-        text.setAttribute("id", "box"+i)
+        items.setAttribute("id", i)
+
+        text.setAttribute("id", "box" + i)
 
 
         let del_button = document.createElement("button")
@@ -38,28 +38,28 @@ function add_tasks(){
         let edit_button = document.createElement("button")
 
         del_button.innerHTML = "DELETE"
-        
+
         edit_button.innerHTML = "EDIT"
-        
+
         text.innerHTML = things_to_do[i]
 
 
-        
+
         task_list.appendChild(items)
 
         items.appendChild(text)
-        
+
         items.appendChild(del_button)
-        
+
         items.appendChild(edit_button)
-        
+
         // event listener for delete button
-        
-        del_button.addEventListener("click",() => {
+
+        del_button.addEventListener("click", () => {
             sample(i)
         })
 
-        edit_button.addEventListener("click",() => {
+        edit_button.addEventListener("click", () => {
             edit(i)
         })
 
@@ -70,22 +70,22 @@ function add_tasks(){
 // function to delete using delete button on the side
 
 
-function sample(count){
+function sample(count) {
 
     // console.log("object"+count);
-    
+
     let dels = document.getElementById(count)
-    things_to_do.splice(count,1)
+    things_to_do.splice(count, 1)
     dels.remove()
 }
 // function to edit using edit button on the side
 
 
-function edit(count){
+function edit(count) {
 
     // console.log("object"+count);
-    
-    let edits = document.getElementById("box"+count)
+
+    let edits = document.getElementById("box" + count)
 
     let a = prompt("enter the new task ")
 
@@ -93,69 +93,85 @@ function edit(count){
 
     things_to_do[count] = a
 
-    
+
 
 }
 
 
 
+function delete_tasks() {
+
+    for (let j = 0; j < things_to_do.length; j++) {
 
 
 
-
-
-
-
-
-
-function delete_tasks(){
-
-    for(let j =0 ; j<things_to_do.length ; j++){
-
-        
-
-        if(task.value == things_to_do[j] ){
+        if (task.value == things_to_do[j]) {
             let list = document.createElement("li")
-            list.innerHTML = things_to_do.splice(j,1)
+            list.innerHTML = things_to_do.splice(j, 1)
             task_list.appendChild(list)
         }
 
-        task_list.innerHTML =""
+        task_list.innerHTML = ""
 
-       
+
 
     }
 
-    for(let i = 0 ; i<things_to_do.length; i++){
+    for (let i = 0; i < things_to_do.length; i++) {
         let list = document.createElement("li")
         list.innerHTML = things_to_do[i]
         task_list.appendChild(list)
-        
+
     }
 }
 
-// function to edit
-
-function edit_tasks(){
-
-
-}
-
-// function for seaarch
-
-function add_task(){ return null}
 
 
 
-// ------------------------------------------------------------------------------------------------------------------------
+
+
+// -------------------------------------------------------------------------------------------------------------------------
 
 // event listeners
 
-task.addEventListener("input",add_task)
 
-add_button.addEventListener("click",add_tasks)
+add_button.addEventListener("click", add_tasks)
 
-delete_button.addEventListener("click",delete_tasks)
+
+
+task.addEventListener("keydown", (e) => {
+
+    if (e.code == 'Enter') {
+
+        add_tasks()
+
+    }
+
+})
+
+
+
+
+// function for space bar edits
+
+document.body.addEventListener("keydown", (e) => {
+
+
+
+    if (e.code == 'Space') {
+
+        console.log("hello");
+        console.log(e);
+
+        let b = prompt("enter your name")
+
+        alert("your name is " + b)
+    }
+
+})
+
+
+
 
 
 
