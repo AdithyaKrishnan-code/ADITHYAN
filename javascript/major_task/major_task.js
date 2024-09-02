d93a9b14f3cb21b028d97738e9f12458003e48ae
AdithyaKrishnan-code <jamesmacaroni69@gmail.com>
2024-09-02 12:35:03 +0530

    completed sigup and login page

--- javascript/major_task/major_task.js
+++ javascript/major_task/major_task.js
@@ -1,15 +1,25 @@
 let home_page = document.getElementById("main_page1") 
 let home_page_content = document.getElementById("basic_info") 
 let nav_bar = document.getElementById("nav_bar")
-let login = document.getElementById("log_in_button")
-let login_page = document.getElementById("login_page")
 let home_page_content_2 = document.getElementById("main_page2")
 
+let login = document.getElementById("log_in_button")
+let login_page = document.getElementById("login_page")
+
+let signup = document.getElementById("signup_button")
+let signup_page = document.getElementById("signup_page")
+
+
+
+
+
 // console.log(login)
-// console.log(home_page_content)
+console.log(signup_page)
 
 
 login_page.style.display = "none"
+signup_page.style.display = "none"
+
 
 login.addEventListener("click",()=>{
 
@@ -20,3 +30,14 @@ login.addEventListener("click",()=>{
     home_page.appendChild(login_page)
 
 })
+
+signup.addEventListener("click",()=>{
+
+
+    home_page_content.style.display = "none"
+    home_page_content_2.style.display = "none"
+    signup_page.style.display = "flex"
+    home_page.appendChild(signup_page)
+
+})
+
