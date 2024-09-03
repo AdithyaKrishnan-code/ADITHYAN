9b347d7b3e8ee85c95243096b73cc33b113ad9df
AdithyaKrishnan-code <jamesmacaroni69@gmail.com>
2024-09-03 17:29:31 +0530

    added deposit popup pages

--- javascript/major_task/major_task.js
+++ javascript/major_task/major_task.js
@@ -5,20 +5,25 @@ let home_page_content_2 = document.getElementById("main_page2")
 
 let login = document.getElementById("log_in_button")
 let login_page = document.getElementById("login_page")
+let deposit_button  = document.getElementById("symbol_icon_deposit")
+let deposit_page_close_button = document.getElementById("close_button")
 
 let signup = document.getElementById("signup_button")
 let signup_page = document.getElementById("signup_page")
+let deposit_page = document.getElementById("deposit_page")
+
 
 
 
 
 
 // console.log(login)
-console.log(signup_page)
+// console.log(signup_page)
 
 
 login_page.style.display = "none"
 signup_page.style.display = "none"
+
 
 
 login.addEventListener("click",()=>{
@@ -26,6 +31,7 @@ login.addEventListener("click",()=>{
 
     home_page_content.style.display = "none"
     home_page_content_2.style.display = "none"
+    signup_page.style.display = "none"
     login_page.style.display = "flex"
     home_page.appendChild(login_page)
 
@@ -36,8 +42,25 @@ signup.addEventListener("click",()=>{
 
     home_page_content.style.display = "none"
     home_page_content_2.style.display = "none"
+    login_page.style.display = "none"
     signup_page.style.display = "flex"
     home_page.appendChild(signup_page)
 
+
 })
+
+deposit_button.addEventListener("click",()=>{
+
+
+    deposit_page.showModal()
+    console.log("hello_wworld")
+})
+
+deposit_page_close_button.addEventListener("click",()=>{
+
+
+    deposit_page.close()
+    console.log("hello_wworld")
+})
+
 
